44e2434559c2715813b74067086938b10d7d93ee
Eduard Khaertdinov <ufo251981@yandex.ru>
2023-04-11 13:18:14 +0300

    fix: Fix all error

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,24 @@
 
-let editButton = document.querySelector('.profile__edit-button_open-popup');
-let profileName = document.querySelector('.profile__info_name_value');
-let profileProfession = document.querySelector('.profile__text_name_value');
+let editButton = document.querySelector('.profile__edit-button');
+let profileName = document.querySelector('.profile__info');
+let profileProfession = document.querySelector('.profile__text');
 let popup = document.querySelector('.popup');
-let formElement = document.querySelector('.popup__container_form_element');
-let closeButton = document.querySelector('.popup__button-close_form_element');
+let formElement = document.querySelector('.popup__form');
+let closeButton = document.querySelector('.popup__button-close');
 
 let nameInput = document.querySelector('.popup__input_user_name'); 
 let jobInput = document.querySelector('.popup__input_user_job'); 
-let buttonSave = document.querySelector('.popup__button-save_close_form');
 
 
 function openPopup() {
-    popup.classList.add('popup_open');
+    popup.classList.add('popup_opened');
     nameInput.value = profileName.textContent;
     jobInput.value = profileProfession.textContent;
 }
 
 
 function closePopup() {
-    popup.classList.remove('popup_open');
+    popup.classList.remove('popup_opened');
 }
 
 
@@ -29,7 +28,6 @@ function handleFormSubmit(evt) {
     profileProfession.textContent = jobInput.value;
     closePopup();
 }
-buttonSave.addEventListener('click', handleFormSubmit);
 editButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
 formElement.addEventListener('submit', handleFormSubmit);
